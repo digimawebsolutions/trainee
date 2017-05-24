3b25f4571c6bee8a6d203dcfc9eb0bba6a92e21f
mailabayno <you@example.com git config --global user.email labaynomariel@gmail.comgit config --global user.name Your>
2017-05-24 18:34:43 +0800

    bago

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,13 +16,9 @@ Route::get('/', function () {
 });
 
 Route::get('/trainee/brain', 'BrainController@index'); //by: brain
-Route::get('/boroms', 'TraineeController@boroms');
-Route::get('/joshua', 'TraineeController@josh');
-Route::get('/mai', 'TraineeController@mai');
 
 
 Route::get('/mai','TraineeController@mai');
-
 Route::get('/trainee/rommel', 'RommelController@index');
 Route::get('/trainee/borroms', 'BorromsController@borroms');
 Route::get('/trainee/renz', 'RenzController@renz');
