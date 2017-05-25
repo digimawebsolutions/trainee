3d87ab41b33cd13a0c53db3d85ea203ad3a5c78e
Your Name <you@example.com>
2017-05-25 13:34:35 -0700

    employees_table

--- app/Http/Controllers/BorromsController.php
+++ app/Http/Controllers/BorromsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use App\Models\Tbl_customer;
+use Redirect;
+use Carbon\Carbon; 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
