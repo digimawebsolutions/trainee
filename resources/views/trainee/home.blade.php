14e183be0aa74022aecebc580105426fa5dc018d
mailabayno <you@example.com git config --global user.email labaynomariel@gmail.comgit config --global user.name Your>
2017-05-31 14:31:21 +0800

    latest

--- resources/views/trainee/home.blade.php
+++ resources/views/trainee/home.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 
 <div class="btn-container">
-	<button style="background-color: lightblue;" class="btn btn" description="Time is gold.">RENZ</button>
-	<button style="background-color: lightblue; " class="btn btn" description="Game is life.">RAYMOND</button>
-	<button style="background-color: lightblue; " class="btn btn" description="Today is your tomorrow.">ROMMEL</button>
-	<button style="background-color: lightblue;" class="btn btn" description="I don't care if you don't love me. Just let me love you.">BRAIN</button>
+	<button style="background-color: lightblue;" class="btn btn" description="Time is bold.">RENZ</button>
+	<button style="background-color: lightblue; " class="btn btn" description="Food is life.">RAYMOND</button>
+	<button style="background-color: lightblue; " class="btn btn" description="Today you're one of the cleaners.">ROMMEL</button>
+	<button style="background-color: lightblue;" class="btn btn" description="I don't care na na na na">BRAIN</button>
 </div>
 <div class="quotation" style="padding-top: 40px;">
 	NONE SELECTED YET
