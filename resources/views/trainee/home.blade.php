2000db70cab59ed9b86b5b1dd9fbb74c77ded391
mailabayno <you@example.com git config --global user.email labaynomariel@gmail.comgit config --global user.name Your>
2017-05-31 14:27:32 +0800

    bago

--- resources/views/trainee/home.blade.php
+++ resources/views/trainee/home.blade.php
@@ -3,9 +3,9 @@
 
 <div class="btn-container">
 	<button style="background-color: lightblue;" class="btn btn" description="Time is gold.">RENZ</button>
-	<button class="btn btn-default" description="Game is life.">RAYMOND</button>
-	<button class="btn btn-default" description="Today is your tomorrow.">ROMMEL</button>
-	<button class="btn btn-default" description="I don't care if you don't love me. Just let me love you.">BRAIN</button>
+	<button style="background-color: lightblue; " class="btn btn" description="Game is life.">RAYMOND</button>
+	<button style="background-color: lightblue; " class="btn btn" description="Today is your tomorrow.">ROMMEL</button>
+	<button style="background-color: lightblue;" class="btn btn" description="I don't care if you don't love me. Just let me love you.">BRAIN</button>
 </div>
 <div class="quotation" style="padding-top: 40px;">
 	NONE SELECTED YET
