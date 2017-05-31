ea54ff990ca8a1141d18e5cc9940221c060a806b
Raymond <raymondgraytorres@gmail.com>
2017-05-31 14:33:05 +0800

    info

--- resources/views/trainee/home.blade.php
+++ resources/views/trainee/home.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 
 <div class="btn-container">
-	<button style="background-color: lightblue;" class="btn btn" description="Time is gold.">RENZ</button>
-	<button class="btn btn-default" description="Game is life.">RAYMOND</button>
-	<button class="btn btn-default" description="Today is your tomorrow.">ROMMEL</button>
-	<button class="btn btn-default" description="I don't care if you don't love me. Just let me love you.">BRAIN</button>
+	<button style="background-color: lightblue;" class="btn btn" description="Time is gold when watching bold.">RENZ</button>
+	<button class="btn btn-default" description="Game is 8.">RAYMOND</button>
+	<button class="btn btn-default" description="Today is your tomorrow 8:00am.">ROMMEL</button>
+	<button class="btn btn-default" description="I don't care if you don't love me. Just let me love you 8times.">BRAIN</button>
 </div>
 <div class="quotation" style="padding-top: 40px;">
 	NONE SELECTED YET
